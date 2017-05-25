a8c48565ca6229fcc56ef0f4a1debd916d1949c0
Malik, Suleman <suleman.malik@Kronos.com>
2017-05-25 15:09:50 -0400

    deleted extra files

--- src/main/java/org/sonarsource/plugins/example/web/ExampleWidget.java
+++ src/main/java/org/sonarsource/plugins/example/web/ExampleWidget.java
@@ -24,41 +24,28 @@ import org.sonar.api.web.Description;
 import org.sonar.api.web.RubyRailsWidget;
 import org.sonar.api.web.UserRole;
 import org.sonar.api.web.WidgetCategory;
-import org.sonar.api.web.WidgetProperties;
-import org.sonar.api.web.WidgetProperty;
-import org.sonar.api.web.WidgetPropertyType;
+
 
 @UserRole(UserRole.USER)
-@Description("Show how to use Ruby Widget API")
+@Description("Sonarqube widget plugin that will display eslint reports on a sonar server dashboard")
+
 @WidgetCategory("Sample")
-@WidgetProperties({
-  @WidgetProperty(key = "param1",
-    description = "This is a mandatory parameter",
-    optional = false),
-  @WidgetProperty(key = "max",
-    description = "max threshold",
-    type = WidgetPropertyType.INTEGER,
-    defaultValue = "80"),
-  @WidgetProperty(key = "param2",
-    description = "This is an optional parameter"),
-  @WidgetProperty(key = "floatprop",
-    description = "test description")
-})
+
 public class ExampleWidget extends AbstractRubyTemplate implements RubyRailsWidget {
 
   @Override
   public String getId() {
-    return "example";
+    return "ESlint report";
   }
 
   @Override
   public String getTitle() {
-    return "Example";
+    return "ESLint Report";
   }
 
   @Override
   protected String getTemplatePath() {
-    return "/example/example_widget.html.erb";
+    return "C:\\Users\\suleman.malik\\Desktop\\parser\\eslint-report-widget.html.erb";
   }
 
 }
