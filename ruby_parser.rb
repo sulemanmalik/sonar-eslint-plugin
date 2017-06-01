8c37d5213fe0619877f0f132a7f9dcea142e1f22
Suleman Malik <suleman.malik@mail.mcgill.ca>
2017-06-01 14:54:21 -0400

    Updating parser to use function
    program can now automatically generate html depending on which file is provided as input (runtime 2x longer, remove redundant code)

--- ruby_parser.rb
+++ ruby_parser.rb
@@ -5,15 +5,244 @@ require 'date'
 
   start = Time.now.to_f
  # templatePath = "#{File.dirname(__FILE__)}/eslint-report-widget.html.erb"
-templatePath = "C:\\Users\\suleman.malik\\Desktop\\eslint-report-widget.html.erb"
+  templatePath = "C:\\Users\\suleman.malik\\Desktop\\eslint-report-widget.html.erb"
 
-f1path = "C:\\Users\\suleman.malik\\Desktop\\web-schedule-eslint.json"
-f2path = "C:\\Users\\suleman.malik\\Desktop\\web-schedule-eslint-2.json"
-f3path = "C:\\Users\\suleman.malik\\Desktop\\web-schedule-eslint-3.json"
-f4path = "C:\\Users\\suleman.malik\\Desktop\\web-schedule-eslint-4.json"
-f5path = "C:\\Users\\suleman.malik\\Desktop\\web-schedule-eslint-5.json"
+  def parser(filePath, reportNumber)
+  file = File.read(filePath)
+  fileHash = JSON.parse(file)
+
+#Calculating numbers of errors and warnings
+  errorCount = 0
+  warningCount = 0
+
+  for i in 0..fileHash.size-1 do
+    errorCount += fileHash[i]['errorCount']
+    warningCount += fileHash[i]['warningCount']
+  end
+  problemCount = errorCount + warningCount
+#Separate errors and warnigns
+  errorStr = ""
+  warningStr = ""
+  i = 0
+  k = 0
+
+  while k<fileHash.size
+    for i in 0..fileHash[k]['messages'].size-1 do
+      if fileHash[k]['messages'][i]['severity'] == 2
+        errorStr += ' ' + fileHash[k]['messages'][i]['ruleId']
+      elsif fileHash[k]['messages'][i]['severity'] == 1
+        warningStr += ' ' + fileHash[k]['messages'][i]['ruleId']
+      end
+    end
+    k+=1
+  end
+
+  errorArray=errorStr.split
+  warningArray=warningStr.split
+
+  errorFrequencyHash = errorArray.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  warningFrequencyHash = warningArray.inject(Hash.new(0)) { |h,v| h[v] +=1; h}
+
+  sortedErrorFrequencyHash = errorFrequencyHash.sort_by{|_key, value| value}
+  sortedWarningFrequencyHash = warningFrequencyHash.sort_by {|_key, value| value}
+
+  top5errors = []
+  top5warnings = []
+
+  (sortedErrorFrequencyHash.size-1).downto(sortedErrorFrequencyHash.size-5){|x| top5errors << sortedErrorFrequencyHash[x]}
+  (sortedWarningFrequencyHash.size-1).downto(sortedWarningFrequencyHash.size-5){|x| top5warnings << sortedWarningFrequencyHash[x]}
+
+  top1Error = top5errors[0][0]
+  top2Error = top5errors[1][0]
+  top3Error = top5errors[2][0]
+  top4Error = top5errors[3][0]
+  top5Error = top5errors[4][0]
+
+  top1ErrorFreq = top5errors[0][1]
+  top2ErrorFreq = top5errors[1][1]
+  top3ErrorFreq = top5errors[2][1]
+  top4ErrorFreq = top5errors[3][1]
+  top5ErrorFreq = top5errors[4][1]
+
+  top1Warning = top5warnings[0][0]
+  top2Warning = top5warnings[1][0]
+  top3Warning = top5warnings[2][0]
+  top4Warning = top5warnings[3][0]
+  top5Warning = top5warnings[4][0]
+
+  top1WarningFreq = top5warnings[0][1]
+  top2WarningFreq = top5warnings[1][1]
+  top3WarningFreq = top5warnings[2][1]
+  top4WarningFreq = top5warnings[3][1]
+  top5WarningFreq = top5warnings[4][1]
+
+#CALCULATING MOST PROBLEMATIC FILES (FILES WITH LARGEST PROBLEM COUNT (ERROR+WARNING COUNT) )
+  for p in 0..fileHash.size-1 do
+    fileHash[p].tap{|hs| hs.delete('messages')}
+  end
+
+  arr = []
+  for s in 0..fileHash.size-1 do
+    arr << fileHash[s].merge(problemCount: fileHash[s]['errorCount']+fileHash[s]['warningCount'])
+  end
+  problemHash = arr.sort_by!{|hsh| hsh[:problemCount] }
+  problemArray=[]
+  (problemHash.size-1).downto(problemHash.size-5){|x| problemArray << problemHash[x]}
+
+  top1ProblemFilePath = problemArray[0]['filePath']
+  top2ProblemFilePath = problemArray[1]['filePath']
+  top3ProblemFilePath = problemArray[2]['filePath']
+  top4ProblemFilePath = problemArray[3]['filePath']
+  top5ProblemFilePath = problemArray[4]['filePath']
+
+  top1ProblemErrors = problemArray[0]['errorCount']
+  top2ProblemErrors = problemArray[1]['errorCount']
+  top3ProblemErrors = problemArray[2]['errorCount']
+  top4ProblemErrors = problemArray[3]['errorCount']
+  top5ProblemErrors = problemArray[4]['errorCount']
+
+  top1ProblemWarnings = problemArray[0]['warningCount']
+  top2ProblemWarnings = problemArray[1]['warningCount']
+  top3ProblemWarnings = problemArray[2]['warningCount']
+  top4ProblemWarnings = problemArray[3]['warningCount']
+  top5ProblemWarnings = problemArray[4]['warningCount']
 
 
+  reportDiv =
+      "
+     <div id='Report#{reportNumber}' class='tabcontent'>
+
+        <div id='top-errors-#{reportNumber}'>
+            <h1 class='text-error'> <i class='fa fa-exclamation-circle' aria-hidden='true'></i> Top Errors </h1>
+            <div class='table'>
+                <table class='summary-table'>
+                    <tr>
+                        <th>Rule</th>
+                        <th>Count</th>
+                    </tr>
+                    <tr>
+                        <td><a href='http://eslint.org/docs/rules/#{top1Error}'>#{top1Error}</a></td>
+                        <td>#{top1ErrorFreq}</td>
+                    </tr>
+                    <tr>
+                        <td><a href='http://eslint.org/docs/rules/#{top2Error}'>#{top2Error}</a></td>
+                        <td>#{top2ErrorFreq}</td>
+                    </tr>
+                    <tr>
+                        <td><a href='http://eslint.org/docs/rules/#{top3Error}'>#{top3Error}</a></td>
+                        <td>#{top3ErrorFreq}</td>
+                    </tr>
+                    <tr>
+                        <td><a href='http://eslint.org/docs/rules/#{top4Error}'>#{top4Error}</a></td>
+                        <td>#{top4ErrorFreq}</td>
+                    </tr>
+                    <tr>
+                        <td><a href='http://eslint.org/docs/rules/#{top5Error}'>#{top5Error}</a></td>
+                        <td>#{top5ErrorFreq}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <br>
+
+        <div id='top-warnings-#{reportNumber}'>
+            <h1 class='text-warning'> <i class='fa fa-arrow-circle-up' aria-hidden='true'></i> Top Warnings </h1>
+            <div class='table'>
+                <table class='summary-table'>
+                    <tr>
+                        <th>Rule</th>
+                        <th>Count</th>
+                    </tr>
+                    <tr>
+                        <td><a href='http://eslint.org/docs/rules/#{top1Warning}'>#{top1Warning}</a></td>
+                        <td>#{top1WarningFreq}</td>
+                    </tr>
+                    <tr>
+                        <td><a href='http://eslint.org/docs/rules/#{top2Warning}'>#{top2Warning}</a></td>
+                        <td>#{top2WarningFreq}</td>
+                    </tr>
+                    <tr>
+                        <td><a href='http://eslint.org/docs/rules/#{top3Warning}'>#{top3Warning}</a></td>
+                        <td>#{top3WarningFreq}</td>
+                    </tr>
+                    <tr>
+                        <td><a href='http://eslint.org/docs/rules/#{top4Warning}'>#{top4Warning}</a></td>
+                        <td>#{top4WarningFreq}</td>
+                    </tr>
+                    <tr>
+                        <td><a href='http://eslint.org/docs/rules/#{top5Warning}'>#{top5Warning}</a></td>
+                        <td>#{top5WarningFreq}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <br>
+
+        <div id='problematic-files-#{reportNumber}'>
+            <h1 class='text-info'> <i class='fa fa-folder-open' aria-hidden='true'></i> Problematic files </h1>
+            <div class='table'>
+                <table class='summary-table'>
+                    <tr>
+                        <th>File Path</th>
+                        <th>Errors</th>
+                        <th>Warnings</th>
+                    </tr>
+                    <tr bgcolor='#F2DEDE'>
+                        <td>#{top1ProblemFilePath}</td>
+                        <td>#{top1ProblemErrors}</td>
+                        <td>#{top1ProblemWarnings}</td>
+                    </tr>
+                    <tr>
+                        <td>#{top2ProblemFilePath}</td>
+                        <td>#{top2ProblemErrors}</td>
+                        <td>#{top2ProblemWarnings}</td>
+                    </tr>
+                    <tr>
+                        <td>#{top3ProblemFilePath}</td>
+                        <td>#{top3ProblemErrors}</td>
+                        <td>#{top3ProblemWarnings}</td>
+                    </tr>
+                    <tr>
+                        <td>#{top4ProblemFilePath}</td>
+                        <td>#{top4ProblemErrors}</td>
+                        <td>#{top4ProblemWarnings}</td>
+                    </tr>
+                    <tr>
+                        <td>#{top5ProblemFilePath}</td>
+                        <td>#{top5ProblemErrors}</td>
+                        <td>#{top5ProblemWarnings}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+  "
+
+  return reportDiv
+end
+
+
+  f1path = "C:\\Users\\suleman.malik\\Desktop\\web-schedule-eslint.json"
+  f2path = "C:\\Users\\suleman.malik\\Desktop\\web-schedule-eslint-2.json"
+  f3path = "C:\\Users\\suleman.malik\\Desktop\\web-schedule-eslint-3.json"
+  f4path = "C:\\Users\\suleman.malik\\Desktop\\web-schedule-eslint-4.json"
+  f5path = "C:\\Users\\suleman.malik\\Desktop\\web-schedule-eslint-5.json"
+
+  div1 = parser(f1path, 1)
+  div2 = parser(f2path, 2)
+  div3 = parser(f3path, 3)
+  div4 = parser(f4path, 4)
+  div5 = parser(f5path, 5)
+
+
+  tab ='"tab"'
+  tablinks = '"tablinks"'
+  defaultOpen ='"defaultOpen"'
+  openCity = %Q("openCity(event, 'Report1')")
+  openCity2 = %Q("openCity(event, 'Report2')")
+  openCity3 = %Q("openCity(event, 'Report3')")
 
   f1 = File.read(f1path)
   f2 = File.read(f2path)
@@ -79,7 +308,6 @@ warningCount5 = 0
   f1_mod_time_month = File.mtime(f1path).month
   f1_mod_time_year = File.mtime(f1path).year
 
-  f1_date_string = #{f1_mod_time_day}/#{f1_mod_time_month}/#{f1_mod_time_year}
 
   f2_mod_time_day = File.mtime(f2path).day
   f2_mod_time_month = File.mtime(f2path).month
@@ -97,55 +325,6 @@ warningCount5 = 0
   f5_mod_time_day = File.mtime(f5path).day
   f5_mod_time_month = File.mtime(f5path).month
   f5_mod_time_year = File.mtime(f5path).year
-
-
-
- # puts Date::MONTHNAMES[old.month]
-
-
-
-
-=begin FILE DIFFERENCES RUBY
-  #f1 = File.read("/Users/sulemanmalik/Desktop/eslint-kronos.json")
-  f1 = File.read("C:\\Users\\suleman.malik\\Desktop\\parser\\web-schedule-eslint.json")
-  #f1=File.read("tmp/eslint-reports/eslint-kronos.json")
-  f1arr = JSON.parse(f1)
-
-  #f2 = File.read("/Users/sulemanmalik/Desktop/eslint-kronos-2.json")
-  f2 = File.read("C:\\Users\\suleman.malik\\Desktop\\eslint-kronos-2.json")
-  #f2=File.read("tmp/eslint-reports/eslint-kronos-2.json")
-  f2arr = JSON.parse(f2)
-
-  if f1arr != f2arr
-    diff= f1arr-f2arr
-  end
-
-  fileArr = []
-  errorArr =[]
-
-  for i in 0..diff.size-1 do
-    fileArr[i]=diff[i]['filePath']
-    errorArr[i] = diff[i]['errorCount']
-  end
-
-  errorCount1=0
-  warningCount1=0
-
-  for i in 0..f1arr.size-1 do
-    errorCount1 += f1arr[i]['errorCount']
-    warningCount1 += f1arr[i]['warningCount']
-  end
-  problemCount1 = errorCount1+warningCount1
-
-  errorCount2=0
-  warningCount2=0
-
-  for i in 0..f2arr.size-1 do
-    errorCount2 += f2arr[i]['errorCount']
-    warningCount2 += f2arr[i]['warningCount']
-  end
-  problemCount2 = errorCount2+warningCount2
-=end
 
 
   #READING JSON FILE, STORING INTO STRING AND PARSING AS A HASH TABLE
@@ -186,6 +365,7 @@ warningCount5 = 0
     end
     k+=1
   end
+
   errorArray=errorStr.split
   warningArray=warningStr.split
 
@@ -330,32 +510,66 @@ warningCount5 = 0
         .graph{
         float: left;
         }
-
-     </style>
-     <script>
-        var acc = document.getElementsByClassName('accordion');
-        var i;
-        for (i = 0; i < acc.length; i++) {
-            acc[i].onclick = function() {
-                this.classList.toggle('active');
-                var panel = this.nextElementSibling;
-                if (panel.style.maxHeight){
-                    panel.style.maxHeight = null;
-                } else {
-                    panel.style.maxHeight = panel.scrollHeight + 'px';
-                }
-            }
+        div.tab {
+            overflow: hidden;
+            background-color: #f1f1f1;
         }
-     </script>
+        /* Style the buttons inside the tab */
 
-      <script>
-                jQuery(document).ready(function(){
-            jQuery('#hideshow').on('click', function(event) {
-                 jQuery('#content').toggle('show');
-            });
-        });
+        div.tab button {
+            background-color: inherit;
+            float: left;
+            border: none;
+            outline: none;
+            cursor: pointer;
+            padding: 8px 15px 28px 15px;
+            transition: 0.3s;
+        }
+        /* Change background color of buttons on hover */
 
-      </script>
+        div.tab button:hover {
+            background-color: #ddd;
+        }
+        /* Create an active/current tablink class */
+
+        div.tab button.active {
+            background-color: #ccc;
+        }
+        /* Style the tab content */
+
+        .tabcontent {
+            display: none;
+            padding: 6px 12px;
+            border-top: none;
+        }
+     </style>
+
+     <script>
+        function openCity(evt, cityName) {
+            // Declare all variables
+            var i, tabcontent, tablinks;
+
+            // Get all elements with class='tabcontent' and hide them
+            tabcontent = document.getElementsByClassName('tabcontent');
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = 'none';
+            }
+
+            // Get all elements with class='tablinks' and remove the class 'active'
+            tablinks = document.getElementsByClassName('tablinks');
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(' active', '');
+            }
+
+            // Show the current tab, and add an 'active' class to the button that opened the tab
+            document.getElementById(cityName).style.display = 'block';
+            evt.currentTarget.className += ' active';
+        }
+    </script>
+<script>
+// Get the element with id='defaultOpen' and click on it
+document.getElementById(#{defaultOpen}).click();
+</script>
 
   </head>
   <body>
@@ -393,115 +607,24 @@ warningCount5 = 0
   });
   </script>
 </div>
-     <h1 class = 'text-error'> <i class='fa fa-exclamation-circle' aria-hidden='true'></i> Top Errors </h1>
-     <div class = 'table'>
-        <table class='summary-table'>
-           <tr>
-              <th>Rule</th>
-              <th>Count</th>
-           </tr>
-           <tr>
-              <td><a href='http://eslint.org/docs/rules/#{top1Error}'>#{top1Error}</a></td>
-              <td>#{top1ErrorFreq}</td>
-           </tr>
-           <tr>
-              <td><a href='http://eslint.org/docs/rules/#{top2Error}'>#{top2Error}</a></td>
-              <td>#{top2ErrorFreq}</td>
-           </tr>
-           <tr>
-              <td><a href='http://eslint.org/docs/rules/#{top3Error}'>#{top3Error}</a></td>
-              <td>#{top3ErrorFreq}</td>
-           </tr>
-           <tr>
-              <td><a href='http://eslint.org/docs/rules/#{top4Error}'>#{top4Error}</a></td>
-              <td>#{top4ErrorFreq}</td>
-           </tr>
-           <tr>
-              <td><a href='http://eslint.org/docs/rules/#{top5Error}'>#{top5Error}</a></td>
-              <td>#{top5ErrorFreq}</td>
-           </tr>
-        </table>
-     </div>
-     <br>
-     <h1 class = 'text-warning'> <i class='fa fa-arrow-circle-up' aria-hidden='true'></i> Top Warnings </h1>
-     <div class = 'table'>
-        <table class='summary-table'>
-           <tr>
-              <th>Rule</th>
-              <th>Count</th>
-           </tr>
-           <tr>
-              <td><a href='http://eslint.org/docs/rules/#{top1Warning}'>#{top1Warning}</a></td>
-              <td>#{top1WarningFreq}</td>
-           </tr>
-           <tr>
-              <td><a href='http://eslint.org/docs/rules/#{top2Warning}'>#{top2Warning}</a></td>
-              <td>#{top2WarningFreq}</td>
-           </tr>
-           <tr>
-              <td><a href='http://eslint.org/docs/rules/#{top3Warning}'>#{top3Warning}</a></td>
-              <td>#{top3WarningFreq}</td>
-           </tr>
-           <tr>
-              <td><a href='http://eslint.org/docs/rules/#{top4Warning}'>#{top4Warning}</a></td>
-              <td>#{top4WarningFreq}</td>
-           </tr>
-           <tr>
-              <td><a href='http://eslint.org/docs/rules/#{top5Warning}'>#{top5Warning}</a></td>
-              <td>#{top5WarningFreq}</td>
-           </tr>
-        </table>
-     </div>
-     <br>
-     <h1 class = 'text-info'> <i class='fa fa-folder-open' aria-hidden='true'></i> Problematic files </h1>
-     <div class = 'table'>
-        <table class='summary-table'>
-           <tr>
-              <th>File Path</th>
-              <th>Errors</th>
-              <th>Warnings</th>
-           </tr>
-           <tr bgcolor='#F2DEDE'>
-              <td>#{top1ProblemFilePath}</td>
-              <td>#{top1ProblemErrors}</td>
-              <td>#{top1ProblemWarnings}</td>
-           </tr>
-           <tr>
-              <td>#{top2ProblemFilePath}</td>
-              <td>#{top2ProblemErrors}</td>
-              <td>#{top2ProblemWarnings}</td>
-           </tr>
-           <tr>
-              <td>#{top3ProblemFilePath}</td>
-              <td>#{top3ProblemErrors}</td>
-              <td>#{top3ProblemWarnings}</td>
-           </tr>
-           <tr>
-              <td>#{top4ProblemFilePath}</td>
-              <td>#{top4ProblemErrors}</td>
-              <td>#{top4ProblemWarnings}</td>
-           </tr>
-           <tr>
-              <td>#{top5ProblemFilePath}</td>
-              <td>#{top5ProblemErrors}</td>
-              <td>#{top5ProblemWarnings}</td>
-           </tr>
-        </table>
-     </div>
-     <br>
+<br>
+<br>
+ <div class=#{tab}>
+        <button class=#{tablinks} onclick=#{openCity}>Report 1</button>
+        <button class=#{tablinks} onclick=#{openCity2}>Report 2</button>
+        <button class=#{tablinks} onclick=#{openCity3}>Report 3</button>
+        <button class=#{tablinks} onclick=#{openCity3}>Report 4</button>
+    </div>
+
+    #{div1}
+    #{div2}
+    #{div3}
+    #{div4}
+
 
   </body>
   "
-=begin FILE DIFFERENCES HTML
 
-     <h1 class = 'text-info'> <i class='fa fa-line-chart' aria-hidden='true'></i> Delta </h1>
-      <p> Previous analysis:<p>
-        <ul>
-          <li>problems: #{problemCount1}</li>
-          <li>warnings: #{warningCount1}</li>
-          <li>errors: #{errorCount1}</li>
-        </ul>
-=end
 
   File.open(templatePath, "w"){|file| file.write(finalErrorStr)}
 
